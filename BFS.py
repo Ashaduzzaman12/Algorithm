7a0280da1c51bf77a1c660a612b5dcdb8b25d7c4
S M ASHADUZZAMAN <137910960+Ashaduzzaman12@users.noreply.github.com>
2024-10-10 22:53:49 +0600

    Create BFS.py

---
+++ BFS.py
@@ -0,0 +1,25 @@
+
+def bfs(graph, state):
+    q = [state]
+    visited = set()
+    while q:
+        node = q.pop(0)
+        if node not in visited:
+            visited.add(node)
+            print(node)
+            for i in graph[node]:
+                if i not in visited:
+                    q.append(i)
+
+graph = {
+    '0': ['1', '2','3'],
+    '1': [],
+    '2': [],
+    '3': ['4'],
+    '4': ['5'],
+    '5': ['6'],
+    '6': ['7'],
+    '7': []
+}
+
+bfs(graph, '0')
